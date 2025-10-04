2c0801f316f2992afb4ec33e635e693ec4e2fb61
Luca Vivona <lucavivona01@gmail.com>
2025-10-03 21:44:55 -0400

    add: utility unflatten dictonary function

---
+++ bindings/python/py/mrkle/utils.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+
+def unflatten(dictionary: dict[str, Any]):
+    """Retrun an unflatten tree."""
+    resultDict: dict[str, Any] = dict()
+    for key, value in iter(dictionary.items()):
+        parts = key.split(".")
+        d = resultDict
+        for part in parts[:-1]:
+            if part not in d:
+                d[part] = dict()
+            if isinstance(d[part], dict):
+                d = d[part]
+
+        d.setdefault(parts[-1], value)
+    return resultDict
